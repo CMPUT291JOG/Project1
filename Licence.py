7f899c11bca1d0e9ed0f798dccbd87b0fef70345
waiyi <waiyi@users.noreply.github.com>
2016-03-07 12:54:32 -0700

    Update Licence.py

--- Licence.py
+++ Licence.py
@@ -13,75 +13,27 @@ import sys
 import cx_Oracle # the package used for accessing Oracle in Python
 import getpass # the package for getting password from user without displaying it
 
-def licence_input(licence_num, sin, lic_class, photo, issuing_date, expiring_date):
+def licence_input(licence_num, sin, licence_class, photo, issuing_date, expiring_date):
   
-  # create and initialize variables
+    # create and initialize variables
     licence_num = ""  	# char
     sin = "" 		# char
-    lic_class = ""	# char
+    licence_class = ""	# char
     photo = null      	# BLOB - from local disk
     issuing_date = null 	# DATE
     expiring_date = null # Date
 
+    # assign inputs to variables
+    # should check input whether they are valid
+    # if not valid, prompt proper message
     licence_num = input ('Enter Licence number: ')
     sin = input ('Enter Social insurance number: ')
-
-    lic_class = input ('Enter licence class')
-
+    licence_class = input ('Enter licence class: ')
     photo = input ('Insert photo ')
-
     issuing_date = input ('Enter issuing date')
+    expiring_date = input ('Enter expiring date ')
 
-    licence_num = input ('Enter expiring date ')
-    
-    return (licence_num, sin, lic_class, photo, issuing_date, expiring_date)
 
-def licence_main():
-  # get username
-	user = input("Username [%s]: " % getpass.getuser())
-	if not user:
-  		user=getpass.getuser()
-	
-	# get password
-	pw = getpass.getpass()
-
-	# The URL we are connnecting to 
-	conString=''+user+'/' + pw +'@gwynne.cs.ualberta.ca:1521/CRS'
-	
-	# SQL statement to execute
-	createLicence = ("create table Licence "
-	"(T_NAME VARCHAR(32) PRIMARY KEY, SUP_ID INTEGER, PRICE FLOAT, SALES INTEGER, TOTAL INTEGER)")
-	"(licence_no. CHAR(15) PRIMARY KEY, sin CHAR(15), class VARCHAR(10), photo BLOB, issuing_date DATE, expiring_date DATE)")
-	
-	try:  
-	  # Establish a connection in Python
-	  connection = cx_Oracle.connect(conString)
-
-	  #create a cursor 
-	  curs = connection.cursor()
-	  
-	  # TODO: if licence table null, create licence table
-	  # curs.execute(createLicence)
-	  #else continue
-
-	  # executing a query to display
-	  curs.execute("SELECT * from autoTransaction")
-	  # get all data and print it
-	  rows = curs.fetchall()
-	  for row in rows:
-	    print(row)
-	
-	  # getting metadata
-	  rows = curs.description
-	  columnCount = len(rows)
-  	  # display column names and type
-	  # (name, type, display_size,internal_size,precision,scale,null_ok)
-  	  for row in rows:
-  	    print(row[0]," ",row[1])
-  	
-  	  # call for input
-  	  licence_input()
-
-if __name__ == "__main__":
-    main()
-
+    # insert values to the table
+    curs.execute("INSERT INTO Licence VALUES(licence_num, sin, lic_class, photo, issuing_date, expiring_date)
+    return 
