15838b88fc1924347ff8b0578909cd91a6c8691e
gemmamarcinkoski <marcinko@ualberta.ca>
2016-03-08 12:58:39 -0700

    Update Vehicle.py

--- Vehicle.py
+++ Vehicle.py
@@ -5,3 +5,13 @@
 # The component shall allow an officer to enter the detailed information about the
 # vehicle and personal information about its new owners, if it is not in the database.
 # You may assume that all the information about vehicle types has been loaded in the initial database.
+
+import sys
+import cx_oracle
+
+def newVehicleRegistration():
+  sin = raw_input
+
+
+if __name__ == "__main__":
+    newVehicleRegistration()
