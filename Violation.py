5d1cfccac1fd2004c5e098a56fdf28592208ffc2
gemmamarcinkoski <marcinko@ualberta.ca>
2016-03-08 15:49:10 -0700

    Update Violation.py

--- Violation.py
+++ Violation.py
@@ -1,2 +1,5 @@
 # Violation Record
 # Gemma
+# This component is used by a police officer to issue a traffic ticket
+# and record the violation. You may assume that all the information about
+# ticket_type has been loaded in the initial database.
