d378b87141bb02b3a7599328b1e1430219ba343a
omokdad <omokdad@yahoo.com>
2016-03-08 13:40:04 -0700

    dunno

--- MainMenu.py
+++ MainMenu.py
@@ -1,2 +1,30 @@
-# MainMenu
-# Omar
+def main() :
+     print ("Programs Available:")
+     print ("\t1. New Vehicle Registration")
+     print ("\t2. Auto Transaction Registration")
+     print ("\t3. Driver Licence Registration")
+     print ("\t4. Violation Record")
+     print ("\t5. Search Engine")
+     while 0 == 0:
+          program = input("Choose Program Number: \n\t")
+          if program == "1":
+               print(1)
+               break
+          elif program == "2":
+               print(2)
+               break
+          elif program == "3":
+               print(3)
+               break
+          elif program == "4":
+               print(4)
+               break
+          elif program == "5":
+               print(5)
+               break
+          else:
+               print ("Invalid Input")
+               
+     
+          
+main()
