184aa7a146c9c4df183d9e069df905fc03af3f3f
waiyi <waiyi@users.noreply.github.com>
2016-03-18 14:25:21 -0600

    finished commenting updated licence

--- Updated_Licence.py
+++ Updated_Licence.py
@@ -15,6 +15,7 @@
 # -Add driver license of a person who already has a license. An error message should be shown.
 # Note: Most python code are referenced from lab files
 ########################################################
+# function for inputting licence registration
 def licence_main(cur):
 
     ## create and initialize variables
@@ -23,21 +24,22 @@ def licence_main(cur):
     #try_again = 0
     #issue = 1
     
-
+    # try to get SIN and in proper format, 15 character or less
     sin = input ('Enter Social insurance number: ')
     while len(sin) > 15:
 	print('Invalid SIN input.')
 	sin = input ('Enter Social insurance number: ')
-	
+
+    # execute query to get licence number by the specified SIN	
     check = "SELECT  licence_no FROM driver_licence WHERE SIN = :sin" 
     cur.execute(check)   
     rows = cur.fetchall()
     
+    # if there's a driver licence by the specified SIN, error message
     if (rows != 0):
-	# if query returns result, the sin exists therefore check licence
 	if row[0] != null:
 	    print ("Licence already exists in database and cannot be edited!")
-	# if no licence exists, just add one
+	# if there's no licence exists, just add one
 	else:
 	    licence_num = input ('Enter Licence number: ')
 	    while len(licence_num) > 15:
@@ -45,8 +47,8 @@ def licence_main(cur):
 		licence_num = input ('Enter Licence number: ')	
 	    # call input function for the rest
 	    licence_input()
-    # if query empty then input for sin and licence 
-    # then call input function for the rest
+    # if there is no SIN and driver licence, then add new SIN and licence  
+    # and then call input function for the rest
     else:
 	sin = input ('Enter Social insurance number: ')
 	while len(sin) > 15:
@@ -60,7 +62,7 @@ def licence_main(cur):
 	licence_input()	
     return
 
-	    
+# for inputting the rest of the input	    
 def licence_input():
     # create and initialize variables
     file = None
@@ -68,30 +70,37 @@ def licence_input():
     try_again = 0
     issue = 1
 
+    # try to get the licence class and in proper format which is 10 character or less
     licence_class = input ('Enter licence class: ')
     while len(licence_class) > 10:
 	print('Invalid licence class iput.')
 	licence_class = input ('Enter Licence class: ')
 
+    # while there's nothng in file
     while file == None :
+    	# try to get a photo and open it
 	photo = input ('Insert photo path: ')
 	try:
 	    file = open (photo,'rb') 
+	# if can't open photo, print error message
 	except IOError :	
 	    print('File not found!')
-	    
+    # read file and then close it	    
     content = file.read ()
     file.close ()
 
-    # input for issuing_date
+    # try to get input for issuing_date in proper format length of 8
     while issue == 1:    
 	issuing_date = input ('Enter issuing date (ddmmyyyy): ')
+	# error message for wrong format
 	if len(issuing_date) != 8:
 	    print("Invalid input")
 	    continue
+	# otherwise try convert to integer
 	else: 
 	    try: 
 		int(issuing_date)
+	    # print error message if failed to convert to integer
 	    except ValueError:
 		print("invalid input")
 		continue
@@ -100,44 +109,53 @@ def licence_input():
     # reset issue to 1
     issue = 1
     
-    # input for expiring_date
+    # try to get input for expiring_date in proper format length of 8
     while issue == 1:    
 	    expiring_date = input ('Enter expiring date (ddmmyyyy): ')
+	    # if not 8 character, invalid input
 	    if len(expiring_date) != 8:
 		print("Invalid input")
 		continue
 	    else: 
+	    	# try to convert character to integer
 		try: 
 		    int(expiring_date)
+		# print error message if failed to convert to integer
 		except ValueError:
 		    print("invalid input")
 		    continue
 	    issue = 0    
 	  
-    # convert string to date 
+    # converting integer to date format 
     datetime.datetime.strptime(issuing_date, "%d%m%Y").date()
     datetime.datetime.strptime(expiring_date, "%d%m%Y").date()
 
+    # get ready to insert values
     blobvar.setvalue(0,content)
     sqlStr = "INSERT INTO Licence VALUES(l_n, sin, l_c, blobData, i_d, e_d"
     cur.setinputsizes (blobData = cx_Oracle.BLOB)
-
+    # try to execute insert to the Licence table
     try:
 	cur.execute (sqlStr, {'l_n': licence_num, 'sin': sin, 'l_c': licence_class, 'blobData': blobvar, 'i_d': issuing_date, 'e_d': expiring_date"})
+    # if result in error, print error message
     except cx_Oracle.DatabaseError as exc:
 	print ("Licence or SIN already in database. \nNo new entry created.")
+	# if failed, check if user wants to try again for new input
 	while True:
 	    try_again = input('Would you like to try new input? (y/n)')
+	    # if yes, call the function again
 	    if try_again == y:
 		licence_input(cur)
 		return
+	    # otherwise error message
 	    elif try_again == n:
 		print("invalid input")
-		
+    # if pass all, input success	
     print("Input Successfull!")
-
+    # check if user want to have another input
     while True:
 	try_again = input("Do you want to input another? (y/n)")
+	# if so, call function again, otherwise exit
 	if try_again == y:
 	    licence_input(cur)
 	    return
